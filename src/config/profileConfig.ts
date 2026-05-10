c3f011c61a81f7630477cbcedc07269ca913d7d2
yinghualian520 <15021498467@139.com>
2026-05-11 00:10:27 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -4,30 +4,5 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 	name: "流萤",
 	bio: "直至一切，燃烧殆尽",
-	links: [
-		{
-			name: "3809077032",
-			icon: "fa7-brands:qq",
-			url: "https://wpa.qq.com/msgrd?v=3&uin=3809077032&site=qq&menu=yes",
-			showName: false,
-		},
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/yinghualian520",
-			showName: false,
-		},
-		{
-			name: "Email",
-			icon: "fa7-solid:envelope",
-			url: "mailto:15021498467@139.com",
-			showName: false,
-		},
-		{
-			name: "RSS",
-			icon: "fa7-solid:rss",
-			url: "/rss/",
-			showName: false,
-		},
-	],
+	links: [],
 };
