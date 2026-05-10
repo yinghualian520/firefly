9abab4b09c24cd440015464e30c7b45a21962164
yinghualian520 <15021498467@139.com>
2026-05-10 17:36:41 +0800

    Update backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -45,12 +45,10 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/ly1.webp",
+			"assets/images/DesktopWallpaper/ly2.avif",
+			"assets/images/DesktopWallpaper/ly3.avif",
+			"assets/images/DesktopWallpaper/ly4.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
