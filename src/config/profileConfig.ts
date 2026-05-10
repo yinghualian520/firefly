a92f063a96ad8debfb7761e92d2d97d796f76e7b
yinghualian520 <15021498467@139.com>
2026-05-10 23:33:21 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "流萤",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "直至一切，燃烧殆尽",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
